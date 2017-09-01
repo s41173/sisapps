0f36ca5426126031a420c8ecbc01fb93b0018a41
s41173 <sanjaya.kiran@gmail.com>
2017-09-01 14:55:08 +0700

    last commit

--- www/js/app.js
+++ www/js/app.js
@@ -16,30 +16,36 @@
 // This file contains your event handlers, the center of your application.
 // NOTE: see app.initEvents() in init-app.js for event handler initialization code.
 
-// Add to index.js or the first page that loads with your app.
-// For Intel XDK and please add this to your app.js.
+function myEventHandler() {
+    "use strict" ;
+
+    var ua = navigator.userAgent ;
+    var str ;
+
+    if( window.Cordova && dev.isDeviceReady.c_cordova_ready__ ) {
+            str = "It worked! Cordova device ready detected at " + dev.isDeviceReady.c_cordova_ready__ + " milliseconds!" ;
+    }
+    else if( window.intel && intel.xdk && dev.isDeviceReady.d_xdk_ready______ ) {
+            str = "It worked! Intel XDK device ready detected at " + dev.isDeviceReady.d_xdk_ready______ + " milliseconds!" ;
+    }
+    else {
+        str = "Bad device ready, or none available because we're running in a browser." ;
+    }
+
+    alert(str) ;
+}
 
 document.addEventListener('deviceready', function () {
-  // Enable to debug issues.
-  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
-  
-  var notificationOpenedCallback = function(jsonData) {
-    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
-  };
-
-  window.plugins.OneSignal
-    .startInit("25cc867f-ba8d-40cc-8838-5c7aaf2f0e1e")
-    .handleNotificationOpened(notificationOpenedCallback)
-    .endInit();
     
   document.addEventListener("backbutton", function(e){
-   if($.mobile.activePage.is('#homepage')){
-       e.preventDefault();
-       navigator.app.exitApp();
-   }
-   else {
-       navigator.app.backHistory()
-   }
+//   if($.mobile.activePage.is('#homepage')){
+//       e.preventDefault();
+//       navigator.app.exitApp();
+//   }
+//   else {
+//       navigator.app.backHistory()
+//   }
+     alert("Hello Aq kembali");
   }, false);
   
   // Call syncHashedEmail anywhere in your app if you have the user's email.
